4a87ada2cfb534f71139de088c5bef8d5fb48784
Pavithra Windows <pavi2098@gmail.com>
2025-11-28 08:37:12 +0530

    test

--- src/main/java/firstTest.java
+++ src/main/java/firstTest.java
@@ -1,28 +1,26 @@
-
-import java.util.regex.Pattern;
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.AriaRole;
-
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class firstTest {
 	public static void main(String[] args) {
-		//tests
 		try (Playwright playwright = Playwright.create()) {
-			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-			BrowserContext context = browser.newContext();
-			Page page = context.newPage();
-			page.navigate("https://playwright.dev");
-			// Expect a title "to contain" a substring.
+			LaunchOptions launch = new LaunchOptions(); // LaunchOptions is a class
+			launch.setChannel("msedge");
+
+			launch.setHeadless(false);
+			launch.setSlowMo(200);
+			Browser browser = playwright.chromium().launch(launch);
+			Page page = browser.newPage();
+			page.navigate("https:amazon.com");
 			System.out.println(page.title());
-			assertThat(page).hasTitle(Pattern.compile("Playwright"));
-		
-			Locator getStartedXpath = page.locator("//a[text()='Get started']");
-			assertThat(getStartedXpath).isVisible();
-			getStartedXpath.click();
-			
-			Locator installationXpath = page.locator("//h1[text()='Installation']");
-			assertThat(installationXpath).isVisible();
+			System.out.println(page.url());
+			page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue shopping")).click();
+			page.pause();
+
+//			browser.close(); playwright.close();
+
 		}
 	}
 }
